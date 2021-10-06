4b22a007ab16b52ff81c9d3e5e7b517cc0264922
Rishabh <karnrishabh25@gmail.com>
2021-10-06 20:08:42 +0530

    Day 12 Commit

--- Day12/Maps.cpp
+++ Day12/Maps.cpp
@@ -7,6 +7,8 @@ using namespace std;
 //in ordered map key stored in sorted order
 //it is being internally implemented using red black tree
 //non contigous memory allocation
+//keys will be unique
+//if we make duplicate key then it will replace old value with new value
 
 int main(){
 
@@ -22,6 +24,11 @@ int main(){
    m[3] ="gfg";
    m[6]; //o(log(n)) //by default empty string and if value is tring then by default 0
 
+
+/*
+m["abcd"] = "dsrf"; Time complexity = s.size() * log(n)
+
+*/
    //method to insert values
    m.insert({4,"rsr"});
    //iterator pointing to key value pair
@@ -32,16 +39,28 @@ int main(){
 
    }
 
-   //traversal using auto
+   //traversal using auto o(n(log(n)))
    for(auto &val : m){
        cout<<val.first<<" "<<val.second<<endl;
    }
 
    //if keys are string then alphabetically sorted
    //to get size of map : m.size() size is dynamic
+   //accessing a value : o(log(n))
 
+   //finding a value
+   auto it = m.find(3);   //return iterator of that value and if value not present then it will return next of end iterator
+   cout<<(*it).first<<" "<<(*it).second;
 
+    //erasing a value
+    m.erase(3);  //taking key value and will delete that O(log(n))
+    auto ite = m.find(5);
+    if(ite !=m.end()){
+    m.erase(ite); //taking iterator value , can't give that iterator which does not exist means if value not exist then iterator will get at next of end and that doesnot exist so it will be segmentation fault
+    }
 
+    // to clear the map
+    m.clear();
    
 return 0;
 }
